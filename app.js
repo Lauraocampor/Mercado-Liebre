9b0b8a7012057527fd8e500eee30c920d4aaae86
Lauraocampor <laura_ocampo5@hotmail.com>
2023-07-13 20:31:21 -0500

    Agregando git ignore

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 //Levantar nuestro servidor
 const port = process.env.PORT || 3001;
-app.listen(port, () =>console.log('Servidor corriendo en el puerto' + process.env.PORT + ' - http://localhost:3001'));
+app.listen(port, () =>console.log('Servidor corriendo en el puerto' + process.env.PORT + ' - http://localhost:' + process.env.PORT));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
